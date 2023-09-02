84328742fbbe5ebe7f0cacf68cb5df2fb8072279
Wesed <dev.wfeduardo@gmail.com>
2023-09-02 00:09:05 -0300

    finalizados o header e o container de titulo

---
+++ src/components/Title/items.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react'
+
+export interface ItemsProps {
+  icon: ReactNode
+  text: string
+  color: string
+}
+
+export function Items({ icon, text, color }: ItemsProps) {
+  return (
+    <div className="flex gap-3 text-base items-center text-base_text min-w-[230px]">
+      <span className={`rounded-full p-2 ${color}`}>
+        {/* <ShoppingCart weight="fill" className="fill-background" /> */}
+        {icon}
+      </span>
+      {text}
+    </div>
+  )
+}
